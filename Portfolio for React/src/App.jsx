b9b0dda08125c061378f1eb1936336345a659663
Zach <zowen17@gmail.com>
2025-02-24 04:47:50 -0500

    no clue anymore

--- Portfolio for React/src/App.jsx
+++ Portfolio for React/src/App.jsx
@@ -7,44 +7,42 @@ import Resume from './components/Resume';
 
 export default function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <header>
-          <h1>Zacharia Owen</h1>
-          <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-              <a className="navbar-brand" href="#">Navbar</a>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/" end>About Me</NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/portfolio">Portfolio</NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/contact">Contact</NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/resume">Resume</NavLink>
-                  </li>
-                </ul>
-              </div>
+    <div className="app-container">
+      <header>
+        <h1>Zacharia Owen</h1>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/" end>About Me</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/portfolio">Portfolio</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/contact">Contact</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/resume">Resume</NavLink>
+                </li>
+              </ul>
             </div>
-          </nav>
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<AboutMe />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/resume" element={<Resume />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<AboutMe />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
